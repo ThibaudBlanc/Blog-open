6f5f30d8762460cd22e228e47234e5dde60948ba
ThibaudBlanc <118885067+ThibaudBlanc@users.noreply.github.com>
2023-05-05 12:09:19 +0200

    Delete Blog directory

--- Blog/core/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $dbhost = "localhost";
-    $dbport = "3306";
-    $dbname = "blogopen";
-
-    $dbuser = "root";
-    $dbpassword = "";
-
-    $dsn = "mysql:host=$dbhost; port=$dbport; dbname=$dbname; charset=utf8";
--- Blog/core/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <ul>
-        <li><a href="">A propos de nous</a></li>
-        <li><a href="">Conditions générales</a></li>
-        <li><a href="">Contact</a></li>
-    </ul>
-</footer>
-</body>
-</html>
